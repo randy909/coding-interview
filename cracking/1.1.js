ba911071ce15ecdb15de6b566a67609fd2358f31
Randy Hancock <randy.hancock@gmail.com>
2013-10-29 11:11:05 -0500

    Implement 1.1 allUnique

--- cracking/1.1.js
+++ cracking/1.1.js
@@ -1,4 +1,12 @@
 
 module.exports = function (str) {
+  var arr = [];
+  for (var idx = 0; idx < str.length; ++idx) {
+    var charCode = str.charCodeAt(idx);
+    if (arr[charCode]) {
+      return false;
+    }
+    arr[charCode] = true;
+  }
   return true;
 }
